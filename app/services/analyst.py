f0514bdfcfd4d4c0d3d760fd9a87343ea853b24d
Μάνος Σφυράκης <sfyr131@gmail.com>
2026-08-01 19:06:07 +0300

    fix langchain imports, pin dependency versions, containerize app with Docker volume for chroma_db persistence

--- app/services/analyst.py
+++ app/services/analyst.py
@@ -1,7 +1,7 @@
 import logging
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from app.db.vector_store import query_documents
 from app.config import settings
 
